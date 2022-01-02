817bc55d4720386aaeb2f01c8941cf16f0ec27bf
SLEEPlESS <rosas_u@ymail.com>
2022-01-01 23:22:00 -0800

    Aded finctiond for player name and console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for laving yourself a message");
+// tyhis will do math and log 20 
+console.log(10 + 10);
+// what is this?
+console.log("Our robots name is" + playerName);
+  
+//   Call to a function/functions
+//   fight();
